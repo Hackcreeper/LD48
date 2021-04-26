c06dcfcfbb4124805f04d871de67164cb90a562f
Jordan Kniest <j.kniest@basecom.de>
2021-04-26 15:41:28 +0200

    Drill head now rotates based on player speed

--- Assets/Scripts/Player/DrillHead.cs
+++ Assets/Scripts/Player/DrillHead.cs
@@ -5,13 +5,18 @@ namespace Player
     public class DrillHead : MonoBehaviour
     {
         public Transform[] targets;
-        public float rotationSpeed = 10;
-    
+        public Control player;
+        public float rotationSpeed = 900;
+
         private void Update()
         {
             foreach (var target in targets)
             {
-                target.Rotate(new Vector3(0, 0, rotationSpeed * Time.deltaTime));   
+                target.Rotate(new Vector3(
+                    0,
+                    0,
+                    rotationSpeed / 8 * player.speed * Time.deltaTime
+                ));
             }
         }
     }
